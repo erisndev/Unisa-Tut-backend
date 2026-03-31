fff6e2a1e6f4486b89dbe4aa8d81da83f8217aa8
Ferdinand <fmmphahle01@gmail.com>
2026-03-31 09:46:08 +0200

    upd

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { 
-  initializePayment, 
-  verifyPayment, 
+const {
+  initializePayment,
+  verifyPayment,
   paymentWebhook,
-  getPaymentStatus 
+  getPaymentStatus,
 } = require("../controllers/paymentController");
 
 // Initialize payment
