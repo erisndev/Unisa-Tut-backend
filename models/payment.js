55e50d8a7732be675c134a24d5cdca94fb76fb46
Ferdinand <fmmphahle01@gmail.com>
2026-03-31 09:41:52 +0200

    upd

--- models/payment.js
+++ models/payment.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 
-const paymentSchema = new mongoose.Schema({
-  order: { type: mongoose.Schema.Types.ObjectId, ref: "Order", required: true },
-  transactionId: String,
-  amount: Number,
-  status: String,
-  provider: { type: String, default: "Paystack" }
-}, { timestamps: true });
+const paymentSchema = new mongoose.Schema(
+  {
+    order: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+    transactionId: String,
+    amount: Number,
+    status: String,
+    provider: { type: String, default: "Paystack" },
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("Payment", paymentSchema);
